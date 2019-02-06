4a46eace51fd54855e7ce46d88f4760d4b92fb92
ambidine <ambi9.3@outlook.fr>
2019-02-06 16:31:48 +0100

    Mis à jour de l'animation de l'ennemi

--- BatailleEnnemi.java
+++ BatailleEnnemi.java
@@ -1,17 +1,21 @@
-package bataille;
+package Bataille;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Vector2f;
 
 public class BatailleEnnemi {
 	private Image ennemi;
 	private int barreVie = 100;
+	private PathAnimation animation;
 
 	public void init() throws SlickException {
-		this.ennemi = new Image("images/ennemi.png");
+		this.ennemi = new Image("image/ennemi.png");
+		this.animation = new PathAnimation(new BezierPath(0, 0, -400, 1, -50, 20, 0, 0), 1000);
 	}
 
 	public void render(GameContainer container, Graphics g) {
-		this.ennemi.drawCentered(container.getWidth() * 3 / 4, container.getHeight() / 2);
+		Vector2f p = animation.currentLocation();
+		ennemi.drawCentered(p.x + container.getWidth() * 3 / 4, p.y + container.getHeight() / 2);
 		
 		g.setColor(new Color(255,255,255));
 	    g.drawRect(container.getWidth() * 3 / 4 - 50, container.getHeight() / 2 - ennemi.getHeight() / 2-30, 100, 20);
@@ -23,7 +27,7 @@ public class BatailleEnnemi {
 	
 	
 	public void attaquer(BatailleJoeur batailleJoeur) {
-		batailleJoeur.setBarreVie(10);
+		this.animation.start();
 	}
 
 	public Image getEnnemi() {
@@ -38,4 +42,9 @@ public class BatailleEnnemi {
 	public void setBarreVie(int degat) {
 		barreVie= barreVie-degat;
 	}
+	
+	public void update(int delta) {
+		  this.animation.update(delta);
+	}
 }
+
