53faae45d6164b931243d91c3c59704645ed2168
ambidine <ambi9.3@outlook.fr>
2019-02-11 14:36:27 +0100

    Mis à jour du point de vie lors d'une bataille

---
+++ Main.java
@@ -0,0 +1,15 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+import sys.StateGame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            new AppGameContainer(new StateGame(), 1200, 600, false).start();
+        } catch (SlickException e) {
+            
+        }
+
+    }
+}
